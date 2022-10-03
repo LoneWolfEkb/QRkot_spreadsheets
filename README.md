1909ce8b63dd0996d2e188be7081043836b5569c
unknown <mturkhan@yandex.ru>
2022-10-03 20:14:59 +0300

    additions

--- README.md
+++ README.md
@@ -13,7 +13,29 @@ python -m venv venv
 ```
 pip install -r requirements.txt
 ```
-Переименуйте  `.env1`. в  `.env`  
+
+Создайте файл .env с настройками, к примеру:
+
+```
+APP_TITLE=Благотворительный фонд котиков
+DESCRIPTION=Сбор пожертвований на нужды хвостатых
+DATABASE_URL=your_database
+SECRET=your_secret
+FIRST_SUPERUSER_EMAIL
+FIRST_SUPERUSER_PASSWORD=
+EMAIL=
+TYPE=service_account
+PROJECT_ID=
+PRIVATE_KEY_ID=
+PRIVATE_KEY=
+CLIENT_EMAIL=
+CLIENT_ID=
+AUTH_URI=
+TOKEN_URI=
+AUTH_PROVIDER_X509_CERT_URL=
+CLIENT_X509_CERT_URL=
+```
+
 Примените миграции
 ```
 alembic upgrade head
--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlalchemy import select
+from sqlalchemy import select, extract
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
@@ -23,6 +23,26 @@ class CRUDCharityProject(CRUDBase):
             where(CharityProject.fully_invested == False)  # noqa
             .order_by(CharityProject.create_date))  # noqa
         return projects.scalars().all()
+
+
+    async def get_projects_by_completion_rate(
+        self,
+        session: AsyncSession()
+    ):
+        projects = await session.execute(
+            select(CharityProject).where(
+                CharityProject.fully_invested
+            ).order_by(
+                extract('year', self.model.close_date) -
+                extract('year', self.model.create_date),
+                extract('month', self.model.close_date) -
+                extract('month', self.model.create_date),
+                extract('day', self.model.close_date) -
+                extract('day', self.model.create_date)
+            )
+        )
+        projects = projects.all()
+        return projects
 
 
 project_crud = CRUDCharityProject(CharityProject)
